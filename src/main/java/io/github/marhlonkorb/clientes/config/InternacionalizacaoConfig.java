de96ec16cab9d8c81b4042d45cc3f09453924e69
Marhlon Korb <marhlonkorb@hotmail.com>
2022-08-30 23:01:55 -0300

    Alteração na classe InternacionalizacaoConfig.java, dependência do hibernate-validation removida pois estava interferindo na inicialização da aplicação.

---
+++ src/main/java/io/github/marhlonkorb/clientes/config/InternacionalizacaoConfig.java
@@ -0,0 +1,33 @@
+package io.github.marhlonkorb.clientes.config;
+
+import org.springframework.context.MessageSource;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.support.ReloadableResourceBundleMessageSource;
+import org.springframework.validation.beanvalidation.LocalValidatorFactoryBean;
+
+import java.nio.charset.Charset;
+import java.util.Locale;
+
+@Configuration
+public class InternacionalizacaoConfig {
+    
+    @Bean
+    public MessageSource messageSource () {
+        ReloadableResourceBundleMessageSource messageSource =
+                new ReloadableResourceBundleMessageSource();
+        messageSource.setBasename("classpath:messages");
+        messageSource.setDefaultEncoding("UTF-8");
+        messageSource.setDefaultLocale(Locale.getDefault());
+        //refresh cache after every 500 mill-secs
+        messageSource.setCacheMillis(500);
+        return messageSource;
+    }
+    
+    @Bean
+    public LocalValidatorFactoryBean getValidator() {
+        LocalValidatorFactoryBean bean = new LocalValidatorFactoryBean();
+        bean.setValidationMessageSource(messageSource());
+        return bean;
+    }
+}
